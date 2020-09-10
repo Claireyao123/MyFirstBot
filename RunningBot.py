e20be167cac00e054902fb7403d7df300233e6cc
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-09-10 16:37:53 -0400

    Support TOKEN file

--- RunningBot.py
+++ RunningBot.py
@@ -1,6 +1,17 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
+import os
+
+def read_file_as_str(file_path):
+    # 判断路径文件存在
+    if not os.path.isfile(file_path):
+        raise TypeError(file_path + " does not exist")
+
+    # 读取文件内容到all_the_text
+    all_the_text = open(file_path).read()
+    # print type(all_the_text)
+    return all_the_text
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
@@ -8,7 +19,9 @@ def start(update, context):
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
-updater = Updater(token='1312914861:AAFcQQT7i5CVNmyvlc11EAXWAK3ufG2DUk8', use_context=True)
+TOKEN=read_file_as_str('TOKEN')
+
+updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 start_handler = CommandHandler('start', start)
