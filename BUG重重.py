79eac6b3f13d42414b2efea3302ace8b8bf2648e
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-09-19 11:13:00 -0400

    修bug

--- BUG重重.py
+++ BUG重重.py
@@ -3,13 +3,9 @@ from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 import os
+import rewards
+import punishs
 
-def rewarded(update, context):
-    reward = ["你得到了90XP, 还不错哦! \nYou got 90XP which was good enough! ", "你得到了40XP, 貌似有点少... \nYou got 40XP, seems a little bit little...", "哈哈，啥都没有！\nHa Ha! Nothing!", "运气也太好了吧？200XP? \nYou are so lucky? 200XP?", "100XP, 嫌少吗？\n100XP, less?"]
-    context.bot.send_message(chat_id=update.effective_chat.id, text=random.choice(reward))
-def punish(update, context):
-    punished = ["啊喔！你丢掉了90XP! \nAh Oh! You lose 90XP！", "去死吧！你失去了80XP! \nBecause of your death, you lose 80XP!", "你没有得到任何惩罚！\nYou didn't get any punish!", "减掉100XP，今天不是你的幸运日哦！\nSubtract 100XP, it's not your day oh!"]
-    context.bot.send_message(chat_id=update.effective_chat.id, text=random.choice(punished))
 def read_file_as_str(file_path):
     # 判断路径文件存在
     if not os.path.isfile(file_path):
@@ -25,7 +21,6 @@ def start(update, context):
 
 def echo(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-    print(random.choice(rewarded))
  
 TOKEN=read_file_as_str('TOKEN')
 
@@ -37,7 +32,7 @@ dispatcher = updater.dispatcher
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 rewards.add_handler(dispatcher)
-punishes.add_handler(dispatcher)
+punishs.add_handler(dispatcher)
 updater.start_polling()
 
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
