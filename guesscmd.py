98bb1905a1b71db5695e85dba8c937779ad4bb69
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-09-26 12:36:37 -0400

    加了读取数字的功能

--- guesscmd.py
+++ guesscmd.py
@@ -1,9 +1,14 @@
 from telegram.ext import Dispatcher,CommandHandler
-
-
+import random
 def guess(update, context):
-    update.message.reply_text("来啦来啦")
+    if len(context.args) == 0:
+        update.message.reply_text("欢迎玩猜数字游戏，请在0-10之间猜一个数字吧！\nWelcome to Guess Number game, please guess between 0 and 10!")
+    else: 
+        number = int(context.args[0])
+        update.message.reply_text("你输入的数字是%s"%number)
+
 
 def add_handler(dp:Dispatcher):
     guess_handler = CommandHandler('guess', guess)
     dp.add_handler(guess_handler)
+
